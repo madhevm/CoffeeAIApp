ba71e2adca06dd517060e7cc4711fa2cfdb7e1ac
madhevm <39713519+madhevm@users.noreply.github.com>
2024-01-21 18:08:14 -0800

    fixed authentication issue

--- public/js/Status.js
+++ public/js/Status.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AccountContext } from "./Account";
+import { useRouter } from "next/router";
 
 const Status = () => {
   const [status, setStatus] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
   const { getSession } = useContext(AccountContext);
+  const router = useRouter();
 
   useEffect(() => {
     getSession().then((session) => {
@@ -13,6 +16,24 @@ const Status = () => {
     });
   }, []);
 
-  return <div>{status ? "You are now logged in enjoy!" : "Please login!"}</div>
+  const handleLoginRedirect = () => {
+    router.push('/login');
+  };
+
+  return(
+  <div>
+    {status ? (
+      "You are now logged in. Enjoy!"
+    ) : (
+      <div className="login-overlay">
+        <div className="login-modal">
+          <p>It looks like you haven't logged into CoffeeAI just yet, please login!</p>
+          <button class="login-modal-button" onClick={handleLoginRedirect}>Go to Login</button>
+        </div>
+      </div>
+    )}
+  </div>
+  );
 };
+
 export default Status;
